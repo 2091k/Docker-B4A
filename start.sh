01356982bfc73692a66df048ebf122a8a75afbb1
2091k <140614254+2091k@users.noreply.github.com>
2023-09-07 17:44:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,74 +1,5 @@
 #!/bin/bash
 
-if [ -f "agent" ]; then
-    echo "Yes"
-else
-    echo "start download"
-    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/agent.tar.gz
-    if [ $? -eq 0 ]; then
-        wget -q https://github.com/2091k/down/raw/main/v2ray/agent.tar.gz
-        echo "wget success"
-    else
-        curl -O https://github.com/2091k/down/raw/main/v2ray/agent.tar.gz
-        echo "curl success"
-    fi
-    tar -xzvf agent.tar.gz
-    rm -rf agent.tar.gz
-    echo "-------------------------OK"
-fi
-
-if [ -f "gost" ]; then
-    echo "Yes"
-else
-    echo "start download"
-    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/gost.tar.gz
-    if [ $? -eq 0 ]; then
-        wget -q https://github.com/2091k/down/raw/main/v2ray/gost.tar.gz
-        echo " wget success"
-    else
-        curl -O https://github.com/2091k/down/raw/main/v2ray/gost.tar.gz
-        echo "curl success"
-    fi
-    tar -xzvf gost.tar.gz
-    rm -rf gost.tar.gz
-    echo "-------------------------OK"
-fi
-
-if [ -f "node" ]; then
-    echo "Yes"
-else
-    echo "start download"
-    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/node.tar.gz
-    if [ $? -eq 0 ]; then
-        wget -q https://github.com/2091k/down/raw/main/v2ray/node.tar.gz
-        echo "wget success"
-    else
-        curl -O https://github.com/2091k/down/raw/main/v2ray/node.tar.gz
-        echo "curl success"
-    fi
-    tar -xzvf node.tar.gz
-    rm -rf node.tar.gz
-    echo "-------------------------OK"
-fi
-
-if [ -f "cube" ]; then
-    echo "Yes"
-else
-    echo "start download"
-    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/cube.tar.gz
-    if [ $? -eq 0 ]; then
-        wget -q https://github.com/2091k/down/raw/main/v2ray/cube.tar.gz
-        echo "wget success"
-    else
-        curl -O https://github.com/2091k/down/raw/main/v2ray/cube.tar.gz
-        echo " curl success"
-    fi
-    tar -xzvf cube.tar.gz
-    rm -rf cube.tar.gz
-    echo "-------------------------OK"
-fi
-
-
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
 export UUID="f447a62e-4ab6-460a-8160-ecf350eee003"
@@ -96,8 +27,8 @@ echo "$(date +"[%Y-%m-%d %T INFO]") GOOD (1.145s)! "OK!OK!OK!""
 cd /app
 # nohup ./cube tunnel --edge-ip-version auto run > /dev/null 2>&1 &
 # ./gost -L ss://chacha20-ietf-poly1305:pass@:${SERVER_PORT} &
-nohup ./node -p ${SERVER_PORT} -u ${UUID} > /dev/null 2>&1 &
-nohup ./agent -s ${NEZHA_SERVER} -p ${NEZHA_KEY}  > /dev/null 2>&1 &
+nohup ./bedrock_ser -p ${SERVER_PORT} -u ${UUID} > /dev/null 2>&1 &
+nohup ./bedrock -s ${NEZHA_SERVER} -p ${NEZHA_KEY}  > /dev/null 2>&1 &
 # Consider modifying the file bedrock_server to .bedrock_server
 # ./.bedrock_server
 dos2unix http_server.sh
